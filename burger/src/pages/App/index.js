bb93b9da6706a6dba9346a80cb56c0fb329d1c2e
shinee51 <shinebayar@alldomain.biz>
2022-01-16 00:53:41 +0800

    Burger project havtasnii zohion baiguulglt

---
+++ burger/src/pages/App/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+
+import Toolbar from "../../components/Toolbar";
+import BurgerBuilder from '../BurgerBuilder';
+
+function App() {
+  return (
+    <div>
+      <Toolbar />
+      <main className='Content'>
+        <BurgerBuilder />
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ burger/src/pages/BurgerBuilder/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <div>
+                <div>Bread</div>
+                <div>Control</div>
+            </div>
+            
+
+            
+            
+        );
+    }
+}
+
+export default BurgerBuilder;
